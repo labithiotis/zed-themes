cff88bc368f1d9fa73b867b3d48d359bf3b07f39
Darren Labithiotis <labithiotis@users.noreply.github.com>
2024-03-16 06:56:05 +0000

    chore: tweak ui for themes preview

--- app/routes/themes._index.tsx
+++ app/routes/themes._index.tsx
@@ -20,16 +20,16 @@ export default function Themes() {
   const { themes } = useLoaderData<LoaderData>();
 
   return (
-    <div className="flex h-screen w-screen bg-stone-300 dark:bg-stone-900 dark:text-zinc-200">
-      <div className="flex flex-col px-6 py-4">
+    <div className="flex h-screen w-full content-stretch bg-stone-300 dark:bg-stone-900 dark:text-zinc-200">
+      <div className="flex w-full flex-col px-6  py-4">
         <span className="mb-2 flex text-xl font-semibold text-zed-800 dark:text-zed-600">
           <span className="flex-1">Zed Themes</span>
           <UiThemeToggle />
         </span>
-        <div className="flex flex-wrap content-start justify-center ">
-          <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3">
+        <div className="flex w-full justify-center">
+          <div className="grid w-full max-w-[1600px] gap-6 sm:grid-cols-2 md:grid-cols-3">
             {themes?.map((theme) => (
-              <a key={theme.id} className="items flex flex-col gap-2" href={'/themes/' + theme.id}>
+              <a key={theme.id} className="items flex min-h-6 flex-col gap-2" href={'/themes/' + theme.id}>
                 <div className="flex flex-col overflow-hidden">
                   <h4 className="text-lg">{theme.name}</h4>
                   <p className="overflow-hidden text-ellipsis text-nowrap text-xs opacity-60">By {theme.author}</p>
