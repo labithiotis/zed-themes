c5a9e82134b21988ba4641ca1125f4e84ce8ba88
Darren Labithiotis <labithiotis@users.noreply.github.com>
2024-03-28 08:27:13 +0000

    chore: ignore build dir for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
     commonjs: true,
     es6: true,
   },
-  ignorePatterns: ['!**/.server', '!**/.client', 'themeValidatorAjv.mjs'],
+  ignorePatterns: ['!**/.server', '!**/.client', 'themeValidatorAjv.mjs', 'build/*'],
 
   // Base config
   extends: ['eslint:recommended'],
