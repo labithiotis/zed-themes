2b5fbbd0561edf9d25ba7a323997b50f895585ad
Darren Labithiotis <labithiotis@users.noreply.github.com>
2024-03-28 08:33:29 +0000

    chore: ignore import for eslint/tsc

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,6 +1,9 @@
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
-import * as build from '../build/server';
 import { getLoadContext } from '../load-context';
+
+// @ts-ignore
+// eslint-disable-next-line import/no-unresolved
+import * as build from '../build/server';
 
 // Build param is not exported from worderd.d.ts, so we need to extract it
 type Build = Parameters<typeof createPagesFunctionHandler>['0']['build'];
