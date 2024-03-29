a71d546f1f21bde3b4f3feb5bd739d1b8da423c3
Darren Labithiotis <labithiotis@users.noreply.github.com>
2024-03-29 15:16:16 +0000

    chore: try with localhost url

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
   },
   projects: [
@@ -28,8 +28,8 @@ export default defineConfig({
   ],
   webServer: {
     timeout: 30000,
-    command: 'pnpm dev --port 3000 --host',
-    url: 'http://127.0.0.1:3000',
+    command: 'pnpm dev --port 3000',
+    url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     stdout: 'pipe',
     stderr: 'pipe',
