8917ed155fe2d04812ec8b9a411402429139438e
Darren Labithiotis <labithiotis@users.noreply.github.com>
2024-03-29 19:04:00 +0100

    feat: add basic e2e tests (#8)
    * feat: add basic e2e tests
    
    * chore: try setting optimizeDeps to resolve error
    
    * chore: revert optimizeDeps and add dev dep
    
    * chore: add build cmd
    
    * chore: increase timeout
    
    * chore: try setting host
    
    * chore: try with localhost url
    
    * chore: add debug logs
    
    * chore: set env inline
    
    * chore: sync themes
    
    * chore: remove webkit
    
    * chore: reduce sync limit for themes
    
    * chore: use key for CF

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { getLoadContext } from './load-context';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3005,
+    port: 3000,
   },
   plugins: [
     remixCloudflareDevProxy({ getLoadContext }),
