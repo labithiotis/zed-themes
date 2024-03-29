a4a3639d396c27c11d4509eed044afefb7bc9741
Darren Labithiotis <labithiotis@users.noreply.github.com>
2024-03-29 17:49:50 +0000

    chore: reduce sync limit for themes

--- dev/syncZedThemesLocal.ts
+++ dev/syncZedThemesLocal.ts
@@ -5,7 +5,7 @@ import { $ } from 'zx';
 $.verbose = false;
 
 const list = await $`npx --yes wrangler kv:key list --binding=themes --preview=false`;
-for (const { name, metadata } of JSON.parse(list.stdout)) {
+for (const { name, metadata } of JSON.parse(list.stdout).slice(0, process.env.SYNC_LIMIT || -1)) {
   const cmd = await $`npx --yes wrangler kv:key get ${name} --binding=themes --preview=false`;
   await $`npx --yes wrangler kv:key put ${name} ${cmd.stdout} --metadata=${JSON.stringify(metadata)} --binding=themes --preview=true --local=true`;
   await $`npx --yes wrangler kv:key put ${name} ${cmd.stdout} --metadata=${JSON.stringify(metadata)} --binding=themes --preview=false --local=true`;
