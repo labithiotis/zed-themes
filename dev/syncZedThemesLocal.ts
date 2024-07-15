3f2bc57e9e6d62da6004e80a1870d1bfd1e8a28d
Darren Labithiotis <labithiotis@users.noreply.github.com>
2024-07-15 20:38:25 +0100

    chore: readd limit

--- dev/syncZedThemesLocal.ts
+++ dev/syncZedThemesLocal.ts
@@ -5,7 +5,7 @@ import { $ } from 'zx';
 $.verbose = false;
 
 const list = await $`npx --yes wrangler kv key list --binding=zed_themes --preview=false`;
-for (const { name, metadata } of JSON.parse(list.stdout)) {
+for (const { name, metadata } of JSON.parse(list.stdout).slice(0, process.env.SYNC_LIMIT || -1)) {
   const cmd = await $`npx --yes wrangler kv key get ${name} --binding=zed_themes --preview=false`;
   await $`npx --yes wrangler kv key put ${name} ${cmd.stdout} --metadata=${JSON.stringify(metadata)} --binding=zed_themes --preview=false --local=true`;
   await $`npx --yes wrangler kv key put ${name} --local=true ${cmd.stdout} --metadata=${JSON.stringify(metadata)} --binding=zed_themes --preview=true --local=true`;
