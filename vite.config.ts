6b8183d089b0e0d8ceaa5f2d72ea24f5fcdde0fe
Darren Labithiotis <labithiotis@users.noreply.github.com>
2024-07-18 12:24:40 +0100

    chore: try install globals

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { vitePlugin as remix, cloudflareDevProxyVitePlugin as remixCloudflareDevProxy } from '@remix-run/dev';
+import { installGlobals } from '@remix-run/node';
 import { defineConfig } from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { getLoadContext } from './load-context';
+
+installGlobals();
 
 // https://vitejs.dev/config/
 export default defineConfig({
