da5310249d078d65c247e8dcc72df7e0a4e45eec
Darren Labithiotis <labithiotis@users.noreply.github.com>
2024-07-18 19:00:55 +0100

    bump

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { vitePlugin as remix, cloudflareDevProxyVitePlugin as remixCloudflareDevProxy } from '@remix-run/dev';
 import { defineConfig } from 'vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { getLoadContext } from './load-context';
@@ -11,7 +10,6 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
-    nodePolyfills({ include: ['buffer'], globals: { Buffer: true } }),
     remixCloudflareDevProxy({ getLoadContext }),
     tsconfigPaths(),
     svgrPlugin({ svgrOptions: { icon: true } }),
