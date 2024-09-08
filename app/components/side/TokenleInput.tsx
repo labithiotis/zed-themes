f8ac3b1655c9f5d7d4c9d10fa5d4a89376fc03ae
Darren Labithiotis <labithiotis@users.noreply.github.com>
2024-09-08 09:12:21 +0100

    chore: only show clear if not disabled

--- app/components/side/TokenleInput.tsx
+++ app/components/side/TokenleInput.tsx
@@ -10,7 +10,7 @@ export default function TokenleInput({ onClear, ...props }: TokenleInputProps) {
   return (
     <div className="group/token-input relative w-full">
       <input {...props} className={cn('pr-10 w-full peer', props.className)} />
-      {!!props.value && (
+      {!props.disabled && !!props.value && (
         <button
           type="button"
           className="hidden group-hover/token-input:block opaccity-80 peer-focus:opaccity-100 absolute right-1 top-1/2 -translate-y-1/2 text-muted-foreground hover:text-primary"
