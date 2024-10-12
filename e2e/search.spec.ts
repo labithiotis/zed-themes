8372767fe65d161ddcd352fb498ee076b76f706a
Darren Labithiotis <labithiotis@users.noreply.github.com>
2024-10-12 19:45:14 +0100

    chore: try with await

--- e2e/search.spec.ts
+++ e2e/search.spec.ts
@@ -3,10 +3,10 @@ import { expect, test } from '@playwright/test';
 test('can search for themes', async ({ page }) => {
   await page.goto('/');
 
-  expect(page.getByTestId('preview-theme').count()).toBeGreaterThan(1);
+  expect(await page.getByTestId('preview-theme').count()).toBeGreaterThan(1);
 
   await page.getByTestId('search-input').click();
   await page.getByTestId('search-input').fill('anya');
 
-  expect(page.getByTestId('preview-theme').count()).toBe(1);
+  expect(await page.getByTestId('preview-theme').count()).toBe(1);
 });
