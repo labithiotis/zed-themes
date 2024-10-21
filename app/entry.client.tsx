2cc4aaf51a201fd7e49a631cb48c34ad6dec1260
Darren Labithiotis <labithiotis@users.noreply.github.com>
2024-10-21 20:58:41 +0200

    Reduce tracking (#117)
    * chore: reduce tracking
    
    * chore: add ui host
    
    * chore: revert api side using proxy

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,20 +1,14 @@
-import { RemixBrowser, useLocation, useMatches } from '@remix-run/react';
+import { RemixBrowser } from '@remix-run/react';
 import * as Sentry from '@sentry/remix';
-import { StrictMode, startTransition, useEffect } from 'react';
+import { StrictMode, startTransition } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 
 Sentry.init({
   dsn: 'https://ad00c9a48ec1d40d8ee464869fe3a993@o4508006947356672.ingest.de.sentry.io/4508009341452368',
   enabled: process.env.NODE_ENV !== 'development',
   sampleRate: 1,
-  tracesSampleRate: 0.5, // 50%
   initialScope: { tags: { server: false } },
   integrations: [
-    Sentry.browserTracingIntegration({
-      useEffect,
-      useLocation,
-      useMatches,
-    }),
     Sentry.extraErrorDataIntegration(),
     Sentry.feedbackIntegration({
       autoInject: false,
@@ -22,13 +16,7 @@ Sentry.init({
       showBranding: false,
       showName: false,
     }),
-    Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
   ],
-  replaysSessionSampleRate: 0.001, // 0.1% of sessions
-  replaysOnErrorSampleRate: 0.01, // 1% of errors
 });
 
 startTransition(() => {
